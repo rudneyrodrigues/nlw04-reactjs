de4e30ca5fea290644e383fb8c4aee895efc620e
Rudney R <rudney.un2016@gmail.com>
2021-03-18 20:38:56 -0300

    Upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
-import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -32,4 +35,16 @@ export default function Home() {
       </CountdownProvider>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  return {
+    props: {
+      level,
+      currentExperience,
+      challengesCompleted
+    }
+  }
 }
